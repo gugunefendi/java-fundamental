9cae2b71574adbd0f051586479097c3f90333a17
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-02-22 22:05:00 +0700

    Practice method

---
+++ practice_method/Main.java
@@ -0,0 +1,14 @@
+package practice_method;
+
+public class Main {
+
+    public static void main(String[] args) {
+        testStatic();
+        Mobil.methodStatic();
+    }
+
+    public static void testStatic() {
+        System.out.println("Test static");
+    }
+    
+}
