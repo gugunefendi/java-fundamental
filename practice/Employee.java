619fb1e0a3701ee90287370f3efe7d69a75c56a3
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-02-24 22:00:22 +0700

    Practice Object and Class

---
+++ practice/Employee.java
@@ -0,0 +1,40 @@
+package practice;
+
+public class Employee {
+
+    // states
+    String name;
+    String position;
+    double salary;
+
+    // no return and have a parameter
+    void setName(String name) {
+        this.name = name;
+    }
+
+    // no return and and have a parameter
+    void setPosition(String position) {
+        this.position = position;
+    }
+
+    // no return and and have a parameter
+    void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    // no return and and have a parameter
+    String getName() {
+        return this.name;
+    }
+
+    // no return and and have a parameter
+    String getPosition() {
+        return this.position;
+    }
+
+    // no return and and have a parameter
+    double getSalary() {
+        return this.salary;
+    }
+    
+}
