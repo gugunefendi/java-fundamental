80db9be7e6bdf6c69876bd47c0c5968c21251f57
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-02-24 12:01:48 +0700

    Overwrite

---
+++ practice4_polymorphism_overwrite/Piano.java
@@ -0,0 +1,10 @@
+package practice4_polymorphism_overwrite;
+
+public class Piano extends AlatMusik {
+
+    @Override
+    public void bunyi() {
+        System.out.println("Ting ting");
+    }
+    
+}
