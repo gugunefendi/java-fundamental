1c6ab00c199d962c0cbd1f1676f06be7deaa5f69
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-03-04 00:19:16 +0700

    GUI more

--- basic/gui/Main.java
+++ basic/gui/Main.java
@@ -12,6 +12,18 @@ public class Main {
 
         double height = Double.parseDouble(JOptionPane.showInputDialog(null, "Enter your height"));
         JOptionPane.showMessageDialog(null, "You are " + height + " cm tall");
+
+        int isHuman = JOptionPane.showConfirmDialog(null, "Are you human?", "Confirmation", JOptionPane.YES_NO_OPTION);
+        if (isHuman == JOptionPane.YES_OPTION) {
+            System.out.println("User confirmed they are human.");
+        } else {
+            System.out.println("User did not confirm they are human.");
+        }
+
+        Object[] options = { "OK", "CANCEL" };
+        JOptionPane.showOptionDialog(null, "Click OK to continue", "Warning",
+             JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE,
+             null, options, options[0]);
     }
 
 }
