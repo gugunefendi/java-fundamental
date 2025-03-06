8fe74df616a1918319d3bb44f80fa6cfb62e0e98
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-03-06 22:55:02 +0700

    Switches

---
+++ basic/switches/Main.java
@@ -0,0 +1,29 @@
+package basic.switches;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // switch = statement that allows a variable to be tested for equality against a list of values
+
+        String day = "Sunday";
+
+        switch (day) {
+            case "Sunday": System.out.println("It is Sunday");
+            break;
+            case "Monday": System.out.println("it is Monday");
+            break;
+            case "Tuesday": System.out.println("it is Tuesday");
+            break;
+            case "Wednesday": System.out.println("it is Wednesday");
+            break;
+            case "Thursday": System.out.println("it is Thursday");
+            break;
+            case "Friday": System.out.println("it is Friday");
+            break;
+            case "Saturday": System.out.println("it is Saturday");
+            break;
+            default: System.out.println("That is not a day!");
+        }
+    }
+    
+}
