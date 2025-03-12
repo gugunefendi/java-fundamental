49fba3bd24bc63d3cb1d7f25bea7adab0d087169
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-03-12 23:58:38 +0700

    Make easy Wrapper Class

--- basic/wrapper_class/Main.java
+++ basic/wrapper_class/Main.java
@@ -34,6 +34,40 @@ public class Main {
           System.out.println(c.doubleValue());
           System.out.println(d.toString());
           System.out.println(e.getClass());
+
+
+          /*
+           * Make to easy Wrapper Class
+           * Wrapper class in Java is Class that use to change data type primitive to Object.
+           * Java provides Wrapper Class to every data type primitive, so it can use in context that require objects (ex.List, Set, Map)
+           * 
+           * Why we need Wrapper Class?
+           * 1. Collection like ArrayList<Integer> can not use primitive type (int), so we need Wrapper Class (Integer)
+           * 2. Generic only supported object type, not a primitive
+           * 3. Can use method (ex. Integer("123")) this is will convert from string to int
+           */
+
+           // Using Wapper Class
+           // 1. Change Primitive to Object (Boxing)
+           int number = 100;
+           Integer objNumber = Integer.valueOf(number); // Boxing (manual)
+           Integer objNumberTwo = number; // Auto-boxing
+           System.out.println(objNumber + objNumberTwo);
+
+           // 2. Change Object to Primitive
+           Integer score = Integer.valueOf(100);
+           int boxingScore = score.intValue(); // boxing manual
+           int boxingScoreTwo = score; // auto-boxing
+           System.out.println(boxingScore + " " + boxingScoreTwo); 
+
+           // 3. Use method in Wrapper Class
+            String numberString = "100";
+            int numberInt = Integer.parseInt(numberString); // Change string to number
+            double decimal = Double.parseDouble("3.14"); // Change string to double
+            System.out.println(numberInt + " " + decimal);
+
+
+
     }
     
 }
