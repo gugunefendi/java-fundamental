85431a37099838515343d579867e33223239a1fd
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-03-18 13:17:04 +0700

    Object

---
+++ basic/object/Car.java
@@ -0,0 +1,19 @@
+package basic.object;
+
+public class Car {
+
+    String make = "Chevrolet";
+    String model = "Corvette";
+    int year =  2020;
+    String color = "Blue";
+    double price = 50000.00;
+
+    void drive() {
+        System.out.println("Your drive the car");
+    }
+
+    void brake() {
+        System.out.println("You step on the brakes");
+    }
+    
+}
