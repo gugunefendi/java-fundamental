baad75426ae437ee6821ff7bacc948f979d0a1ef
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-04-09 10:32:13 +0700

    Boolean

---
+++ hh_boolean/Main.java
@@ -0,0 +1,30 @@
+package hh_boolean;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        /*
+        Boolean
+        Boolean adalah tipe data yang mengembalikkan true atau false
+
+        Expresi Boolean
+        Expresi boolean mengembalikkan nilai true atau false
+        contoh:
+        if(10 > 5) { // ini akan mengmbalikkan nilai true
+            // code here
+        }
+        */
+
+        boolean isJavaFun = true;
+        boolean isFishTasty = false;
+        System.out.println(isJavaFun); // true
+        System.out.println(isFishTasty); // false
+
+        // boolean expression
+        if(10 > 5) { // true
+            System.out.println("10 > 5");
+        }
+
+    }
+
+}
