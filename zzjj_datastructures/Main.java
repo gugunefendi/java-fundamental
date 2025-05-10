db77e7e9bcccabb196b846b5c388a66e5975c0c4
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-05-10 22:19:48 +0700

    Array List

---
+++ zzjj_datastructures/Main.java
@@ -0,0 +1,42 @@
+package zzjj_datastructures;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+         * Data Structure / Struktur Data
+         * Struktur data adalah cara untuk menyimpan data dan mengatur data sehingga kamu dapat menggunakannya secara efisien.
+         * 
+         * Java meneyediakan banyak struktur data bawaan dalam package java.util.
+         * 
+         * Beberapa struktur data yang paling umum digunakan:
+         * 1. ArrayList
+         * 2. LinkedList
+         * 3. Hashmap
+         * 4. HashSet
+         * 
+         */
+
+         /*
+          * ArrayList
+          * ArrayList adalah array yang dapat diubah ukurannya secara dinamis.
+          * Memungkinkan kita menyimpan dan mengaksesnya berdasarkan indeks.
+          */
+
+          ArrayList<String> cars = new ArrayList<String>();
+          cars.add("Toyota");
+          cars.add("Honda");
+          cars.add("Suzuki");
+          System.out.println(cars);
+
+
+
+
+
+
+    }
+    
+}
