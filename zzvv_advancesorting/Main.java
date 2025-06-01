ca3375d657f655d91b9cda3d7f1c1952daf5225d
Gunawan Efendi <gunawanefendi32@gmail.com>
2025-06-01 22:47:48 +0700

    Comparator Number

--- zzvv_advancesorting/Main.java
+++ zzvv_advancesorting/Main.java
@@ -44,6 +44,23 @@ public class Main {
             System.out.println(c.brand + " " + c.model + " " + c.year);
         }
 
+        // Comparator juga dapat digunakan untuk membuat sorting khusus untuk String dan number
+        // contoh berikut ini kita akan menggunakan comparator untuk mencantumkan semua angka genap sebelum ganjil
+        ArrayList<Integer> myNumbers = new ArrayList<Integer>();
+        myNumbers.add(33);
+        myNumbers.add(15);
+        myNumbers.add(20);
+        myNumbers.add(34);
+        myNumbers.add(8);
+        myNumbers.add(12);
+
+        Comparator<Integer> myComparator = new SortEvenFirst();
+        Collections.sort(myNumbers, myComparator);
+
+        for (int i: myNumbers) {
+            System.out.println(i);
+        }
+
 
     }
         
